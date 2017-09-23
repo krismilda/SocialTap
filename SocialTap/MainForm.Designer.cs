a28727f308b2f0a42d1c0923ea1b1fe55f9b7836
Milda Jocaite <milda.jocaite@gmail.com>
2017-09-23 12:55:24 +0300

    Create new Location package

--- SocialTap/MainForm.Designer.cs
+++ SocialTap/MainForm.Designer.cs
@@ -32,44 +32,56 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.lblNameText = new System.Windows.Forms.Label();
-            this.lblPercentageText = new System.Windows.Forms.Label();
-            this.lblPercentage = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.errorMessage = new System.Windows.Forms.Label();
+            this.lblAddressText = new System.Windows.Forms.Label();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblPercentageText = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.imageBox = new Emgu.CV.UI.ImageBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.lblAddressText = new System.Windows.Forms.Label();
-            this.lblAddress = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.btnFindMap = new System.Windows.Forms.Button();
+            this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel7 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel8 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.tableLayoutPanel6.SuspendLayout();
+            this.tableLayoutPanel7.SuspendLayout();
+            this.tableLayoutPanel8.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
             this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 323F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.tableLayoutPanel1.Controls.Add(this.btnOpenFile, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.lblNameText, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblName, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.errorMessage, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.lblPercentageText, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.lblAddressText, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.lblPercentage, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.lblAddress, 1, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblPercentageText, 0, 3);
+            this.tableLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.85449F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(463, 105);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(350, 91);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // btnOpenFile
@@ -77,7 +89,7 @@
             this.btnOpenFile.Location = new System.Drawing.Point(4, 4);
             this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(86, 28);
+            this.btnOpenFile.Size = new System.Drawing.Size(84, 26);
             this.btnOpenFile.TabIndex = 0;
             this.btnOpenFile.Text = "OPEN";
             this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -85,34 +97,18 @@
             // 
             // lblNameText
             // 
+            this.lblNameText.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblNameText.AutoSize = true;
-            this.lblNameText.Location = new System.Drawing.Point(3, 37);
+            this.lblNameText.Location = new System.Drawing.Point(3, 34);
             this.lblNameText.Name = "lblNameText";
             this.lblNameText.Size = new System.Drawing.Size(46, 19);
             this.lblNameText.TabIndex = 4;
             this.lblNameText.Text = "Name";
             // 
-            // lblPercentageText
-            // 
-            this.lblPercentageText.AutoSize = true;
-            this.lblPercentageText.Location = new System.Drawing.Point(3, 81);
-            this.lblPercentageText.Name = "lblPercentageText";
-            this.lblPercentageText.Size = new System.Drawing.Size(134, 19);
-            this.lblPercentageText.TabIndex = 2;
-            this.lblPercentageText.Text = "Percentage of Liquid";
-            // 
-            // lblPercentage
-            // 
-            this.lblPercentage.AutoSize = true;
-            this.lblPercentage.Location = new System.Drawing.Point(143, 81);
-            this.lblPercentage.Name = "lblPercentage";
-            this.lblPercentage.Size = new System.Drawing.Size(0, 19);
-            this.lblPercentage.TabIndex = 3;
-            // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(143, 37);
+            this.lblName.Location = new System.Drawing.Point(143, 34);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(0, 19);
             this.lblName.TabIndex = 5;
@@ -128,18 +124,54 @@
             this.errorMessage.TabIndex = 1;
             this.errorMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // lblAddressText
+            // 
+            this.lblAddressText.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblAddressText.AutoSize = true;
+            this.lblAddressText.Location = new System.Drawing.Point(3, 53);
+            this.lblAddressText.Name = "lblAddressText";
+            this.lblAddressText.Size = new System.Drawing.Size(60, 19);
+            this.lblAddressText.TabIndex = 6;
+            this.lblAddressText.Text = "Address";
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(143, 72);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(0, 19);
+            this.lblPercentage.TabIndex = 3;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(143, 53);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(0, 19);
+            this.lblAddress.TabIndex = 7;
+            // 
+            // lblPercentageText
+            // 
+            this.lblPercentageText.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblPercentageText.AutoSize = true;
+            this.lblPercentageText.Location = new System.Drawing.Point(3, 72);
+            this.lblPercentageText.Name = "lblPercentageText";
+            this.lblPercentageText.Size = new System.Drawing.Size(134, 19);
+            this.lblPercentageText.TabIndex = 2;
+            this.lblPercentageText.Text = "Percentage of Liquid";
+            // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.AutoSize = true;
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.imageBox, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(2, 127);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 103);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(463, 476);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(351, 365);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // imageBox
@@ -148,8 +180,8 @@
             this.imageBox.Location = new System.Drawing.Point(4, 4);
             this.imageBox.Margin = new System.Windows.Forms.Padding(4);
             this.imageBox.Name = "imageBox";
-            this.imageBox.Size = new System.Drawing.Size(455, 468);
-            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox.Size = new System.Drawing.Size(343, 357);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBox.TabIndex = 2;
             this.imageBox.TabStop = false;
             // 
@@ -157,30 +189,89 @@
             // 
             this.openFileDialog.FileName = "FileName";
             // 
-            // lblAddressText
+            // tableLayoutPanel4
             // 
-            this.lblAddressText.AutoSize = true;
-            this.lblAddressText.Location = new System.Drawing.Point(3, 61);
-            this.lblAddressText.Name = "lblAddressText";
-            this.lblAddressText.Size = new System.Drawing.Size(60, 19);
-            this.lblAddressText.TabIndex = 6;
-            this.lblAddressText.Text = "Address";
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.41457F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 283F));
+            this.tableLayoutPanel4.Controls.Add(this.cmbType, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.btnFindMap, 0, 0);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.13044F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(355, 31);
+            this.tableLayoutPanel4.TabIndex = 2;
             // 
-            // lblAddress
+            // cmbType
             // 
-            this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(143, 61);
-            this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(0, 19);
-            this.lblAddress.TabIndex = 7;
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Restaurant",
+            "Bar",
+            "Cafe"});
+            this.cmbType.Location = new System.Drawing.Point(75, 3);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(194, 27);
+            this.cmbType.TabIndex = 1;
+            this.cmbType.Text = "Restaurant";
+            // 
+            // btnFindMap
+            // 
+            this.btnFindMap.Location = new System.Drawing.Point(3, 3);
+            this.btnFindMap.Name = "btnFindMap";
+            this.btnFindMap.Size = new System.Drawing.Size(66, 25);
+            this.btnFindMap.TabIndex = 0;
+            this.btnFindMap.Text = "FIND";
+            this.btnFindMap.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel6
+            // 
+            this.tableLayoutPanel6.ColumnCount = 2;
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.Controls.Add(this.tableLayoutPanel7, 0, 0);
+            this.tableLayoutPanel6.Controls.Add(this.tableLayoutPanel8, 1, 0);
+            this.tableLayoutPanel6.Location = new System.Drawing.Point(0, 1);
+            this.tableLayoutPanel6.Name = "tableLayoutPanel6";
+            this.tableLayoutPanel6.RowCount = 1;
+            this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel6.Size = new System.Drawing.Size(734, 482);
+            this.tableLayoutPanel6.TabIndex = 3;
+            // 
+            // tableLayoutPanel7
+            // 
+            this.tableLayoutPanel7.ColumnCount = 1;
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel7.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel7.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel7.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel7.Name = "tableLayoutPanel7";
+            this.tableLayoutPanel7.RowCount = 2;
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.03093F));
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.96907F));
+            this.tableLayoutPanel7.Size = new System.Drawing.Size(359, 474);
+            this.tableLayoutPanel7.TabIndex = 0;
+            // 
+            // tableLayoutPanel8
+            // 
+            this.tableLayoutPanel8.ColumnCount = 1;
+            this.tableLayoutPanel8.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel8.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel8.Location = new System.Drawing.Point(370, 3);
+            this.tableLayoutPanel8.Name = "tableLayoutPanel8";
+            this.tableLayoutPanel8.RowCount = 2;
+            this.tableLayoutPanel8.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.403361F));
+            this.tableLayoutPanel8.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.59664F));
+            this.tableLayoutPanel8.Size = new System.Drawing.Size(361, 476);
+            this.tableLayoutPanel8.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 475);
-            this.Controls.Add(this.tableLayoutPanel2);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(735, 480);
+            this.Controls.Add(this.tableLayoutPanel6);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
@@ -188,10 +279,13 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel6.ResumeLayout(false);
+            this.tableLayoutPanel7.ResumeLayout(false);
+            this.tableLayoutPanel7.PerformLayout();
+            this.tableLayoutPanel8.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -209,6 +303,12 @@
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblAddressText;
         private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button btnFindMap;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel7;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel8;
     }
 }
 
