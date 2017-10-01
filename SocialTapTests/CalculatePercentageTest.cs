a1e2f3465f0bc6dbe0349bac096984d982035034
Vytautas Krivickas <vytautas1996@gmail.com>
2017-10-01 19:49:59 +0300

    Add test for simpleImageAnalysis.

---
+++ SocialTapTests/CalculatePercentageTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using social_tap;
+
+namespace SocialTapTests
+{
+    [TestClass]
+    public class CalculatePercentageTest
+    {
+        [TestMethod]
+        public void IsSimpleImagePercentageCalculationCorrect()
+        {
+            //Arrange
+            Bitmap bmp = new Bitmap(@"../../TestData/Glass1.bmp");
+            var simga = new SimpleImageAnalysis(bmp);
+
+            //Act
+            var percentage = simga.CalculatePercentageOfLiquid();
+
+            //Assert
+            Assert.AreEqual(percentage, 62);
+        }
+    }
+}
