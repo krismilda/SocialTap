2b0f080263aef814fc7460e9c6326032084aa412
Milda Jocaite <milda.jocaite@gmail.com>
2017-10-02 16:23:19 +0300

    Add test for CalculatingAverage

---
+++ SocialTapTests/CalculatingAverageTest.cs
@@ -0,0 +1,26 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialTapTests
+{
+    [TestClass]
+    public class CalculatingAverageTest
+    {
+        [TestMethod]
+        public void IsAverageTenAndSixCorrect()
+        {
+            //Arrange
+
+            //Act
+            var average = CalculatingAverage.GetAverage(10,6);
+
+            //Assert
+            Assert.AreEqual(average, 1,67);
+        }
+    }
+}
