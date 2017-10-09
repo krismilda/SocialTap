9b767c20bc19f88db17b9c0f3bd7f261edf41f98
Milda Jocaite <milda.jocaite@gmail.com>
2017-10-09 11:41:18 +0300

    Change getting restaurant information (fix mismatch)

--- Database/RestaurantData/RestaurantInformation.cs
+++ Database/RestaurantData/RestaurantInformation.cs
@@ -1,4 +1,6 @@
-﻿using Services;
+﻿using Emgu.CV;
+using Services;
+using Services.ImageAnalysis;
 using social_tap;
 using System;
 using System.Collections;
@@ -7,6 +9,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Database
 {
@@ -18,12 +21,13 @@ namespace Database
         public String Address { get; set; }
         public int Percentage { get; set; }
 
-        public async Task GetRestaurantInformation(Bitmap bitmap)
+        public async Task GetRestaurantInformation(String path, PictureBox imageBox2)
         {
             GooglePlacesApiData googleApiData = new GooglePlacesApiData();
             GooglePlacesApiResponse responseData = await googleApiData.GetApiResponseData("food");
-            SimpleImageAnalysis imageInformation = new SimpleImageAnalysis(bitmap);
-            int percentageOfLiquid = imageInformation.CalculatePercentageOfLiquid();
+            RealPhotoAnalysis rpa = new RealPhotoAnalysis(new Image<Emgu.CV.Structure.Bgr, byte>(path));
+            imageBox2.Image = rpa.VisualRepresentation.Bitmap;
+            int percentageOfLiquid = rpa.Percentage;
             Date = DateTime.Today;
             Name = responseData.results[0].name;
             Address = responseData.results[0].vicinity;
