19ff149fafc4c6cde4352863d34ea0b7561d9f23
Monika Cepulyte <monikutecepulyte97@gmail.com>
2017-10-22 12:49:04 +0300

    Add Xamarin.Forms

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello world", "ok");
+        }
+
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
