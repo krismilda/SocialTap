975c0e91d82a7aba9c81d963ff3693c279cdeac4
Monika Cepulyte <monikutecepulyte97@gmail.com>
2017-11-05 22:44:43 +0200

    Add API and DataModel projects. Create BMI API.

---
+++ API/Controllers/TopRestaurantsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class TopRestaurantsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
