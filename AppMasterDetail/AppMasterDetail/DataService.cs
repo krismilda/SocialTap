6941bb163d14591535e3c64084c3f4973693c92b
milda.jocaite <milda.jocaite@gmail.com>
2017-12-02 11:38:21 +0200

    Publish web api to Azure

--- AppMasterDetail/AppMasterDetail/DataService.cs
+++ AppMasterDetail/AppMasterDetail/DataService.cs
@@ -6,7 +6,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Tweetinvi.Models;
 
 namespace AppMasterDetail
 {
@@ -31,17 +30,41 @@ namespace AppMasterDetail
 
         public static async Task <Restaurant> GetTweetList ()
         {
+          
+
+            Restaurant r = new Restaurant();
+            r.Name = "Musu25 restoranas";
+            r.Address = "Adresas";
+            r.Percentage = 2;
+
+            using (var client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(r);
+
+                var httpContent = new StringContent(content, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://drinkly.azurewebsites.net/api/Tweet", httpContent);
+            }
+
+
             using (HttpClient client = new HttpClient())
             {
+                try
+                {
                 client.MaxResponseContentBufferSize = 256000;
 
-                var uri = new Uri("http://localhost:58376/api/Tweet");
+                var uri = new Uri("http://drinkly.azurewebsites.net/api/Tweet");
 
                 var response = await client.GetAsync(uri);
                 string json = response.Content.ReadAsStringAsync().Result;
                 var data = JsonConvert.DeserializeObject<Restaurant>(json);
                 return data;
+                }
+                catch(Exception e)
+                {
+                    string s = e.ToString();
+                }
             }
+            return null;
         }
 
         public static async Task<Restaurant> GetRestaurant()
---
+++ DataAccess/SocialTap2.cs
@@ -0,0 +1,23 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class SocialTap2: DbContext
+    {
+        public SocialTap2() : base("name = SocialTapDB")
+        {
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Scan> Scans { get; set; }
+
+    }
+
+    
+    
+}
