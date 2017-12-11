a0373535abee2edea50d2b13f3f3b3d89443c7d3
milda.jocaite <milda.jocaite@gmail.com>
2017-12-11 13:24:54 +0200

    Add delete old data

---
+++ API/Controllers/DeleteOldDataController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess;
+using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class DeleteOldDataController : ApiController
+    {
+        SocialTapContext context = new SocialTapContext();
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var scans = context.Scans.ToList();
+            foreach(Scan scan in scans)
+            {
+                if(DateTime.Compare(scan.Date.AddDays(30), DateTime.Today)<0)
+                {
+                    context.Scans.Remove(scan);
+                    context.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+    }
+}
